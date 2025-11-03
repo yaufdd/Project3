7a72b18badd43faaea3ad345f82fed8da9b68889
yaufdd <ericbamba@yandex.ru>
2025-11-03 19:52:46 +0300

    Add: redis get token functions

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -15,19 +15,28 @@ func NewCache(rdb *redis.Client) *Redis {
 	return &Redis{rClient: rdb}
 }
 
-func (r *Redis) SaveToken(ctx context.Context, tokenHash string, ttl time.Duration) error {
-	return r.rClient.Set(ctx, tokenHash, "whitelist", ttl).Err()
+func (r *Redis) SaveToken(ctx context.Context, token string, ttl time.Duration) error {
+	return r.rClient.Set(ctx, token, "whitelist", ttl).Err()
 }
 
-func (r *Redis) BanToken(ctx context.Context, tokenHash string, ttl time.Duration) error {
-	return r.rClient.Set(ctx, tokenHash, "blacklist", ttl).Err()
+func (r *Redis) GetToken(ctx context.Context, token string, ttl time.Duration) (string, error) {
+	val, err := r.rClient.Get(ctx, token).Result()
+	if err != nil {
+		return "", err
+	}
+	return val, err
+
 }
 
-func (r *Redis) IsTokenBanned(ctx context.Context, tokenHash string) bool {
-	list, err := r.rClient.Get(ctx, tokenHash).Result()
+func (r *Redis) BanToken(ctx context.Context, token string, ttl time.Duration) error {
+	return r.rClient.Set(ctx, token, "blacklist", ttl).Err()
+}
+
+func (r *Redis) IsTokenBanned(ctx context.Context, token string) bool {
+	list, err := r.rClient.Get(ctx, token).Result()
 	if err != nil {
 		return false
-	} else if list == "+" {
+	} else if list == "blacklist" {
 		return true
 	}
 	return false
